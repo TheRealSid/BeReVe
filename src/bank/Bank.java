2000fbedf953265c8952a5b2e7ef9bf96ed7c6f5
Thomas Maier <thomas@thomas-maier.net>
2011-09-15 16:54:59 +0200

    hfffhh

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -1,28 +1,32 @@
 package bank;
 
-import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import java.util.Random;
-
-import bank.security.SKonto;
 
 import security.SecurityCallback;
 import security.SecurityProxy;
+import world.SPerson;
+import bank.security.SKonto;
 
 public class Bank extends SecurityCallback{
-	private SKonto konto;
+	//private SKonto konto;
 	private boolean confirm;
 	private Random rnd;
 	private Integer tan;
+	private HashMap<SPerson, SKonto> konten;
 	
 	public Bank() {
 		rnd = new Random();
-		
+		konten = new HashMap<SPerson, SKonto>();
 	}
 
-	public SKonto getKonto() {
+	/*public SKonto getKonto() {
 		if(konto != null) return konto;
-		else return (SKonto) SecurityProxy.newInstance(new Konto(500), this, SKonto.class);
-	}
+		else {
+			konto = (SKonto) SecurityProxy.newInstance(new Konto(500), this, SKonto.class);
+			return konto;
+		}
+	}*/
 
 	public void doPrivileged(SKonto konto){
 		tan = createTan();
@@ -32,7 +36,13 @@ public class Bank extends SecurityCallback{
 		System.out.println("Bank2: "+geld);
 	}
 
-	
+	public void createKonto(SPerson person, int guthaben) {
+		konten.put(person, (SKonto) SecurityProxy.newInstance(new Konto(guthaben), this, SKonto.class));
+	}
+
+	public SKonto getKonto(SPerson person) {
+		return konten.get(person);
+	}
 
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,11 @@
 package main;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.security.GuardedObject;
-
-import bank.Bank;
-import bank.security.SKonto;
 
 import security.SecurityCallback;
 import security.SecurityObjectException;
+import world.Stadt;
+import bank.Bank;
+
 
 
 
@@ -28,14 +26,12 @@ public class Main extends SecurityCallback{
 	 * @throws NoSuchFieldException 
 	 */
 	public static void main(String[] args) throws SecurityObjectException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, NoSuchFieldException {
-		Main m = new Main();
-		m.doSomething();
+		Stadt stadt = new Stadt();
+		stadt.leben();
 	}
 	
 	public void doSomething(){
 		Bank bank = new Bank();
-		SKonto konto= bank.getKonto();
-		bank.doPrivileged(konto);
 		//SecurityException
 //		try{
 //			konto.getGeld(this);
@@ -43,9 +39,6 @@ public class Main extends SecurityCallback{
 //			System.out.println("SOE caught");
 //		}
 //		konto.setGeld(5000,this);
-		konto.setGeld(10,this,createTan());
-		Integer foo = konto.getGeld(this,createTan());
-		System.out.println(foo);
 	}
 	@Override
 	public boolean equals(Object obj) {
