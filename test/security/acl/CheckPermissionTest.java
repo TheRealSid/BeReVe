ed3da62b568e1ca0613b330a8f774017f6c0266e
Dominik Gaetjens <Dominik.Gaetjens@googlemail.com>
2011-10-10 19:13:10 +0200

    some really great unit tests

---
+++ test/security/acl/CheckPermissionTest.java
@@ -0,0 +1,118 @@
+package security.acl;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import security.SecurityCallback;
+import security.SecurityProxy;
+import security.acl.PermissionEntity.Scope;
+import world.Person;
+import world.SPerson;
+import world.Stadt;
+
+
+public class CheckPermissionTest extends SecurityCallback{
+
+	ACL acl;
+	Person bob;
+	SPerson sBob;
+	Person alice;
+	SPerson sAlice;
+	Person eve;
+	SPerson sEve;
+	Stadt stadt;
+	Stadt evilStadt;
+	
+	@Before
+	public void tearUp(){
+		acl = ACL.getInstance();
+		 bob = new Person("bob");
+		 alice = new Person("alice");
+		 eve = new Person("eve");
+		 stadt = new Stadt();
+		 evilStadt = new Stadt();
+		 sBob = (SPerson) SecurityProxy.newInstance(bob, this, SPerson.class);
+		 sEve = (SPerson) SecurityProxy.newInstance(eve, alice, SPerson.class);
+		 sAlice = (SPerson) SecurityProxy.newInstance(alice, this, SPerson.class);
+	}
+	
+	@Test
+	public void testCantAddPermissionForUnownedObject(){
+		PermissionEntity entity = new PermissionEntity(Scope.INSTANCE,Scope.INSTANCE,null,null,false).addMethod("doSex").addTarget(sEve).addCaller(bob);
+		assertFalse(acl.addPermission(entity,createTan(),this));
+		assertFalse(acl.checkPermission("doSex", eve, bob));
+		
+	}
+	@Test
+	public void testCheckPermissionIdentity2Identity() {
+		PermissionEntity entity = new PermissionEntity(Scope.INSTANCE,Scope.INSTANCE,null,null,false).addMethod("doSex").addTarget(sBob).addCaller(alice);
+		acl.addPermission(entity,createTan(),this);
+		assertTrue(acl.checkPermission("doSex",bob,alice));
+		assertFalse(acl.checkPermission("getKonto",bob,alice));
+		assertFalse(acl.checkPermission("doSex",bob,eve));
+	}
+	@Test
+	public void testCheckPermissionIdentity2Identity2() {
+		PermissionEntity entity = new PermissionEntity(Scope.INSTANCE,Scope.INSTANCE,null,null,false).addMethod("doSex").addMethod("getKonto").addTarget(sBob).addCaller(alice);
+		acl.addPermission(entity,createTan(),this);
+		assertTrue(acl.checkPermission("doSex",bob,alice));
+		assertTrue(acl.checkPermission("getKonto",bob,alice));
+		assertFalse(acl.checkPermission("doSex",bob,eve));
+	}
+	@Test
+	public void testCheckPermissionIdentity2Identity3() {
+		PermissionEntity entity = new PermissionEntity(Scope.INSTANCE,Scope.INSTANCE,null,null,false).addMethod("doSex").addMethod("getKonto").addTarget(sBob).addCaller(alice).addCaller(eve);
+		acl.addPermission(entity,createTan(),this);
+		assertTrue(acl.checkPermission("doSex",bob,alice));
+		assertTrue(acl.checkPermission("getKonto",bob,alice));
+		assertTrue(acl.checkPermission("doSex",bob,eve));
+	}
+	@Test
+	public void testCheckPermissionClass2Class() {
+		PermissionEntity entity = new PermissionEntity(Scope.CLASS,Scope.CLASS,Person.class,Stadt.class,false).addMethod("doSex");
+		acl.addPermission(entity,createTan(),this);
+		assertFalse(acl.checkPermission("doSex",bob,alice));
+		assertFalse(acl.checkPermission("doSex",bob,eve));
+		assertTrue(acl.checkPermission("doSex",bob,stadt));
+		assertFalse(acl.checkPermission("getKonto",bob,alice));
+		assertFalse(acl.checkPermission("getKonto",bob,stadt));
+		assertFalse(acl.checkPermission("getKonto",alice,stadt));
+	}
+	@Test
+	public void testCheckPermissionClass2Identity() {
+		PermissionEntity entity = new PermissionEntity(Scope.INSTANCE,Scope.CLASS,null,Stadt.class,false).addMethod("getKonto").addTarget(sBob);
+		acl.addPermission(entity,createTan(),this);
+		assertTrue(acl.checkPermission("getKonto",bob,stadt));
+		assertFalse(acl.checkPermission("getKonto",bob,eve));
+		assertFalse(acl.checkPermission("getKonto",eve,stadt));
+	}
+	@Test
+	public void testCheckPermissionIdentity2Class() {
+		PermissionEntity entity = new PermissionEntity(Scope.CLASS,Scope.INSTANCE,Person.class,null,false).addMethod("getKonto").addCaller(stadt);
+		acl.addPermission(entity,createTan(),this);
+		assertTrue(acl.checkPermission("getKonto",bob,stadt));
+		assertTrue(acl.checkPermission("getKonto",eve,stadt));
+		assertTrue(acl.checkPermission("getKonto",alice,stadt));
+		assertFalse(acl.checkPermission("getKonto",alice,eve));
+		assertFalse(acl.checkPermission("getKonto",alice,evilStadt));
+	}
+	@Test
+	public void testCheckPermissionAllMethods(){
+		PermissionEntity entity = new PermissionEntity(Scope.INSTANCE,Scope.INSTANCE,null,null,true).addTarget(sBob).addCaller(alice);
+		acl.addPermission(entity,createTan(),this);
+		assertTrue(acl.checkPermission("doSex",bob,alice));
+		assertTrue(acl.checkPermission("getKonto",bob,alice));
+		assertFalse(acl.checkPermission("doSex",bob,eve));
+	}
+	@Test
+	public void testCheckPermissionAllMethodsClass2Class(){
+		PermissionEntity entity = new PermissionEntity(Scope.CLASS,Scope.CLASS,Person.class,Stadt.class,true);
+		acl.addPermission(entity,createTan(),this);
+		assertTrue(acl.checkPermission("doSex",bob,stadt));
+		assertTrue(acl.checkPermission("getKonto",bob,stadt));
+		assertFalse(acl.checkPermission("doSex",bob,eve));
+	}
+
+}
